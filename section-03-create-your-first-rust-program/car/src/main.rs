751855cacccceb125925b322aab47a75c1d4382b
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-11-12 09:35:50 +0100

    Complete program that builds 3 cars.

--- section-03-create-your-first-rust-program/car/src/main.rs
+++ section-03-create-your-first-rust-program/car/src/main.rs
@@ -15,5 +15,36 @@ enum Transmission {
 }
 
 fn main() {
-    println!("Hello, world!");
+    // We have orders for three new cars!
+    // We'll declare a mutable car variable and reuse it for all the cars
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!(
+        "Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.colour, car.transmission, car.convertible, car.mileage
+    );
+
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!(
+        "Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.colour, car.transmission, car.convertible, car.mileage
+    );
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!(
+        "Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}",
+        car.colour, car.transmission, car.convertible, car.mileage
+    );
+}
+
+// Build a "Car" by using values from the input arguments
+// - Colour of car (String)
+// - Transmission type (enum value)
+// - Convertible (boolean, true if car is a convertible)
+fn car_factory(colour: String, transmission: Transmission, convertible: bool) -> Car {
+    Car {
+        colour: colour,
+        transmission: transmission,
+        convertible: convertible,
+        mileage: 0,
+    }
 }
