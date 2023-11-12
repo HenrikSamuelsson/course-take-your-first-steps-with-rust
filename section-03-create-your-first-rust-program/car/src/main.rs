4154c6bf7d1e0de6c1e482e42144a8ce88ccd027
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-11-12 08:52:52 +0100

    Add Car struct and transmission enum.

--- section-03-create-your-first-rust-program/car/src/main.rs
+++ section-03-create-your-first-rust-program/car/src/main.rs
@@ -1,3 +1,19 @@
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    colour: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
 fn main() {
     println!("Hello, world!");
 }
