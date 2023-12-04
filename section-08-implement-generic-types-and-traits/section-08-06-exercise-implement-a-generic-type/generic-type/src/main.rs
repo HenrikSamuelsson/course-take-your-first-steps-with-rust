0a3ef278872b1e92822e2af5917fc371adbef32c
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-12-04 06:07:11 +0100

    Add exercise solution.

--- section-08-implement-generic-types-and-traits/section-08-06-exercise-implement-a-generic-type/generic-type/src/main.rs
+++ section-08-implement-generic-types-and-traits/section-08-06-exercise-implement-a-generic-type/generic-type/src/main.rs
@@ -1,3 +1,18 @@
+struct Jar<T> {
+    value: T,
+}
+
+impl<T> Jar<T> {
+    pub fn new(value: T) -> Self {
+        Jar { value }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Jar::new(42).value, 42);
+    assert_eq!(Jar::new(3.14).value, 3.14);
+    assert_eq!(Jar::new("Foo").value, "Foo");
+    assert_eq!(Jar::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(Jar::new(true).value, true);
+    assert_eq!(Jar::new(-12).value, -12);
+    assert_eq!(Jar::new(Some("text")).value, Some("text"));
 }
