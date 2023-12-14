914a9822eddfcefee0b18cf810e1a2aabff3db06
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-12-14 07:00:26 +0100

    Add start of exercise.
    Something is currently broken so tests do not run as expected.

---
+++ section-10/unit-05/exercise_write_documentation_tests/src/lib.rs
@@ -0,0 +1,43 @@
+/// This function divides two numbers.
+///
+/// # Example #1: 10 / 2 == 5
+///
+/// ```
+/// let result = exercise_write_documentation_tests::div(10, 2);
+/// assert_eq!(result, 5);
+/// ```
+///
+/// # Example #2: 6 / 3 = 2
+///
+/// ```
+/// let result = exercise_write_documentation_tests::div(6, 3);
+/// assert_eq!(result, 2);
+/// ```
+///
+/// # Panics
+///
+/// The function panics if the second argument is zero.
+///
+pub fn div(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        panic!("Divide-by-zero error");
+    }
+    a / b
+}
+
+/// This function subtracts two numbers.
+///
+/// # Example #1: 9 - 2 == 7
+///
+/// ```
+/// TODO: Write this doctest!
+/// ```
+///
+/// # Example #2: 6 - 9 == -3
+///
+/// ```
+/// TODO: Write this doctest!
+/// ```
+pub fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
